2bea28ba60d68fd4f60a77cd36f591b24673d82d
acain589 <acain589@gmail.com>
2022-11-05 17:02:26 -0400

    Rearranged project back into reasonable scope

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('What type of diet do you adhere to?', name => {
-    if (name === "Vegetarian" || name === "Carnivore" || name === "Pescitarian") {
-        //let something = name
-        readline.close();
-    } else {
-        console.log(`Please enter Vegetarian, Carnivore or Pescitarian`)
-        readline.question('What tpe of diet do you adhere to?')
-    }
-    
-  });
+const prompt = require('prompt-sync')();
+let meat = ['Burger', 'Shrimp', 'Lobster', 'Chicken', 'Steak'];
+let drink = ['Soda', 'Water', 'Wine', 'Beer'];
+let dessert = ['Cake', 'Ice Cream', 'Cookie', 'Fruit Cup', 'Donuts']
 
-  test
+let meatRandom = Math.floor(Math.random() * meat.length);
+let drinkRandom = Math.floor(Math.random() * drink.length);
+let dessertRandom = Math.floor(Math.random() * dessert.length);
+
+let meatResponse = meat[meatRandom];
+let drinkResponse = drink[drinkRandom];
+let dessertResponse = dessert[dessertRandom];
+
+
+const namePrompt = prompt('Enter your name and your random message will appear ');
+console.log(`Hey there ${namePrompt} You\'re meal recommendation for the day is a meat choice of ${meatResponse}. \nWith a drink choice of ${drinkResponse} \nand dessert choice of ${dessertResponse} \n\nHope you enjoy!!:-)`);
